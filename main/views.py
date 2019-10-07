feb3963fa2ab7bf2925b8145f62e8db78df3fcf2
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-10-07 21:42:47 +0530

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    context = {
+        "title": "Hacktober-Fest",
+    }
+    return render(request,'index.html',context)
